46ed5d57a800afed46aeec136cc4af6a36b7e785
lewibt01 <lewibt01@students.ipfw.edu>
2018-11-28 15:31:26 -0500

    Added a Query Folder to store SQL Queries and added a table to the database

---
+++ SQLQueries/CreateTables.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE StoreItems (
+	Id				int				Not Null	IDENTITY	PRIMARY KEY,
+	ProductName		nvarchar(60)	Not Null,
+	ProductType		nvarchar(60)	Not Null,
+	Price			decimal			Not Null	DEFAULT 0.0,
+	Descriptor		nvarchar(200)
+);
